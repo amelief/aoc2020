be02296f216c9dc92557806d2a3a8284564c7506
Amelie Freeman <amelie@amelierosalyn.com>
2020-12-03 14:16:43 +0000

    Add day 1

---
+++ advent_1.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+NUMBERS = File.read('input/1_input.txt').split("\n").map(&:to_i)
+
+PARTS = 2
+TARGET = 2020
+
+OUTPUT = '%s match. Multiplied together, they make: %d'
+
+def loop_numbers(&block)
+  NUMBERS.each do |n|
+    yield(n)
+  end
+end
+
+PARTS.times do |i|
+  loop_numbers do |num|
+    loop_numbers do |num2|
+      if i == 0 # part 1
+        if num + num2 == TARGET
+          puts sprintf(OUTPUT, "#{num} + #{num2}", (num * num2))
+          break
+        end
+      elsif i == 1 # part 2
+        loop_numbers do |num3|
+          if num + num2 + num3 == TARGET
+            puts sprintf(OUTPUT, "#{num} + #{num2} + #{num3}", (num * num2 * num3))
+            break
+          end
+        end
+      end
+    end
+  end
+end
