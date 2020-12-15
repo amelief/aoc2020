ceb5650804e158c599120c64abf29631839f1326
Amelie Freeman <amelie@amelierosalyn.com>
2020-12-15 13:19:22 +0000

    Completed day 15

--- advent_15.rb
+++ advent_15.rb
@@ -1,46 +1,34 @@
 STARTING_NUMBERS = [2,0,1,9,5,19]
 
-$numbers = {}
-
-def number_spoken?(num)
-  $numbers.keys.include?(num) && $numbers[num].size > 1
-end
-
-def number_mentioned_difference(num)
-  $numbers[num].sort.last - $numbers[num].sort[-2]
-end
-
-def register_number(num, turn)
-  $numbers[num] ||= [];
-  $numbers[num] << turn
-
-  while($numbers[num].size > 2) do
-    $numbers[num].delete($numbers[num][0])
-  end
-end
-
 def calculate_next_number(finish)
   current_number = 0
+  numbers = {}
 
   1.upto(finish) do |turn|
-    if turn % 10000 == 0
+    if turn % 1_000_000 == 0
       puts "Turn: #{turn}"
     end
-    previous_number = current_number.dup
+    previous_number = current_number.clone
 
     if turn <= STARTING_NUMBERS.size
       current_number = STARTING_NUMBERS[turn - 1]
     elsif turn == STARTING_NUMBERS.size + 1
       current_number = 0
     else
-      if number_spoken?(previous_number)
-        current_number = number_mentioned_difference(previous_number)
+      if numbers.key?(previous_number) && numbers[previous_number].size > 1
+        current_number = 
+          numbers[previous_number].sort.last - numbers[previous_number].sort[-2]
       else
         current_number = 0
       end
     end
 
-    register_number(current_number, turn)
+    numbers[current_number] ||= [];
+    numbers[current_number] << turn
+
+    while(numbers[current_number].size > 2) do
+      numbers[current_number].delete(numbers[current_number][0])
+    end
   end
 
   puts "Number is: #{current_number}"
